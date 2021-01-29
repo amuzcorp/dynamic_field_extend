cb91392cf6fed81788696d578b457957fd975304
xocode <overcode@amuz.co.kr>
2021-01-29 16:38:52 +0900

    캘린더 DF 값 없을때 오류 수정

--- src/DynamicFields/Calendar/Skins/AmuzDefault/views/edit.blade.php
+++ src/DynamicFields/Calendar/Skins/AmuzDefault/views/edit.blade.php
@@ -4,9 +4,11 @@
 <div class="xe-form-group xe-dynamicField">
     <label class="xu-form-group__label __xe_df __xe_df_text __xe_df_text_{{ $key['ca'] }}">{{ xe_trans($config->get('label')) }}</label>
     <div>
+        @if(count($values) > 0)
         <div class="xu-form-group__box" style="float:left; width:50%; padding-right:10px;">
             <input type="text" id="{{ $key['ca'] }}_start" name="{{ $key['ca'] }}[]" class="xe-form-control" placeholder="시작일" value="{{ $values[0] }}">
         </div>
+        @endif
         @if(count($values) > 1)
         <div class="xu-form-group__box" style="float:right; width:50%; padding-left:10px; @if($config->get('date_type') == 'single') display:none @endif ">
             <input type="text" id="{{ $key['ca'] }}_end" name="{{ $key['ca'] }}[]" class="xe-form-control" placeholder="종료일" value="{{ $values[1] }}">
--- src/DynamicFields/Calendar/Skins/AmuzDefault/views/show.blade.php
+++ src/DynamicFields/Calendar/Skins/AmuzDefault/views/show.blade.php
@@ -1,9 +1,11 @@
 <div class="xe-form-group xe-dynamicField">
     <label class="__xe_df __xe_df_text __xe_df_text_{{$config->get('id')}}">{{xe_trans($config->get('label'))}}</label>
     @if($config->get('date_type') == 'single')
+        @if(count($values) > 0)
         <div>
             <span>{{ $values[0] }}</span>
         </div>
+        @endif
     @else
         @if(count($values) > 1)
         <div>
