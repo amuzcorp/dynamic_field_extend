ad81027f94f64751dc27edd237a9ed8c70c3f09a
zoozkwon <!qkqhfksek7603>
2021-04-16 16:37:10 +0900

    CellPhoneNumberSkin 위치 이동

--- src/DynamicFieldSkins/CellPhoneNumberSkin/views/edit.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="col-md-2">
-    <label class="form-label">{{xe_trans($config->get('label'))}}</label>
-</div>
-<div class="col-md">
-    <input type="text" class="form-control" name="{{$key['cell_phone_number']}}" value="{{$data['cell_phone_number']}}" data-valid-name="{{ xe_trans($config->get('label')) }}"
-           @if ($config->get('placeholder', '') != '') placeholder="{{xe_trans($config->get('placeholder'))}}" @endif />
-</div>
--- src/DynamicFieldSkins/CellPhoneNumberSkin/views/show.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="row mb-3">
-    <div class="col-md-2">
-        <label for="UserInfoForm-Number" class="form-label">{{xe_trans($config->get('label'))}}</label>
-    </div>
-    <div class="col-md">
-        <input type="text" class="form-control" name="{{$key['cell_phone_number']}}" value="{{$data['cell_phone_number']}}" />
-    </div>
-</div>
