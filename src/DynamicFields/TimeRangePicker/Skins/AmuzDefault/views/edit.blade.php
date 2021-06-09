320b6e75630df05e4a79af4b6d2f511a9b48e523
zoozkwon <!qkqhfksek7603>
2021-06-09 17:34:38 +0900

    timeRangePicker js화, jqeury 클래스이름으로 동작하도록 수정

--- src/DynamicFields/TimeRangePicker/Skins/AmuzDefault/views/edit.blade.php
+++ src/DynamicFields/TimeRangePicker/Skins/AmuzDefault/views/edit.blade.php
@@ -6,19 +6,18 @@
     <label class="xu-form-group__label __xe_df __xe_df_text __xe_df_text_{{ $key['start'] }}">{{ xe_trans($config->get('label')) }}</label>
     <div>
         <div class="xu-form-group__box" style="float:left; width:50%; padding-right:10px;">
-            <input type="text" id="trp_{{ $key['start'] }}_start" name="{{ $key['start'] }}" class="xe-form-control" placeholder="{{ $config->get('date_type') == 'single' ? '날짜선택' : '시작일' }}" value="{{ $data['start'] }}">
+            <input type="text" id="trp_{{ $key['start'] }}_start" name="{{ $key['start'] }}" class="xe-form-control amuz-time-range-picker" placeholder="{{ $config->get('date_type') == 'single' ? '날짜선택' : '시작일' }}" value="{{ $data['start'] }}">
         </div>
         <div class="xu-form-group__box" style="float:right; width:50%; padding-left:10px; @if($config->get('time_type') == 'single') display:none @endif ">
-            <input type="text" id="trp_{{ $key['end'] }}_end" name="{{ $key['end'] }}" class="xe-form-control" placeholder="종료시간" value="{{ $data['end'] }}">
+            <input type="text" id="trp_{{ $key['end'] }}_end" name="{{ $key['end'] }}" class="xe-form-control amuz-time-range-picker" placeholder="종료시간" value="{{ $data['end'] }}">
         </div>
         <div style="clear:both"></div>
     </div>
 </div>
 
 <script type="text/javascript">
-
     $(document).ready(function(){
-        $('#trp_{{ $key['start'] }}_start').timepicker({
+        $('.amuz-time-range-picker').timepicker({
             timeFormat: 'HH:mm',
             interval: 10,
             minTime: '00:00',
@@ -40,34 +39,6 @@
                     end_time.setMinutes(end.substring(2));
 
                     $('#trp_{{ $key['trp'] }}_start').val(end_time.format('HH:mm'));
-
-                    alert('시작시간은 종료시간보다 작아야 합니다.');
-                }
-                --}}
-            }
-        });
-        $('#trp_{{ $key['end'] }}_end').timepicker({
-            timeFormat: 'HH:mm',
-            interval: 10,
-            minTime: '00:00',
-            maxTime: '23:50',
-            defaultTime: '{{ $data['end'] }}',
-            startTime: '09:00',
-            dynamic: false,
-            dropdown: true,
-            scrollbar: true,
-            change: function(time) {
-                {{--
-                let start = $('#trp_{{ $key['trp'] }}_start').val();
-                start = start.replaceAll(':','');
-                let end = time.format('HHmm');
-
-                if(start > end) {
-                    let start_time = new Date();
-                    start_time.setHours(end.substring(0, 2) + 1);
-                    start_time.setMinutes(end.substring(2));
-
-                    $('#trp_{{ $key['trp'] }}_end').val(start_time.format('HH:mm'));
 
                     alert('시작시간은 종료시간보다 작아야 합니다.');
                 }
