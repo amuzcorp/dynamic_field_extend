358b83a8d7804e2049f58f4ddf3d0d91baf84cc9
zoozkwon <!qkqhfksek7603>
2021-09-28 15:49:21 +0900

    시간타입 선택 시 show 수정

--- src/DynamicFields/Calendar/Skins/AmuzDefault/views/show.blade.php
+++ src/DynamicFields/Calendar/Skins/AmuzDefault/views/show.blade.php
@@ -1,9 +1,16 @@
 <div class="xe-form-group xe-dynamicField">
     <label class="__xe_df __xe_df_text __xe_df_text_{{$config->get('id')}}">{{xe_trans($config->get('label'))}}</label>
+
     @if($config->get('date_type') == 'single')
-        <div>
-            <span>{{ $data['start'] }}</span>
-        </div>
+        @if($config->get('time_type') == 'single')
+            <div>
+                <span>{{ $data['start'] }}</span>
+            </div>
+        @else
+            <div>
+                <span>{{ $data['start'] }}</span><span> ~ </span><span>{{ $data['end'] }}</span>
+            </div>
+        @endif
     @else
         <div>
             <span>{{ $data['start'] }}</span><span> ~ </span><span>{{ $data['end'] }}</span>
