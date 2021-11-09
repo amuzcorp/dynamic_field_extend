b06cabb29bc5cc6e86090b462b78196814b90b62
zoozkwon <!qkqhfksek7603>
2021-11-09 12:35:40 +0900

    foreach 에러 수정

--- src/DynamicFields/CategoryInput/views/list-item.blade.php
+++ src/DynamicFields/CategoryInput/views/list-item.blade.php
@@ -3,9 +3,13 @@
         @if($json_obj != null)
         <div class="{{ $key }}">
             @php $category_field_items = json_dec($json_obj); @endphp
-            @foreach($category_field_items as $category_field_item)
-                <span class="xe-badge btn-default">{{ $category_field_item }}</span>
-            @endforeach
+            @if(is_array($category_field_items) == true)
+                @foreach($category_field_items as $category_field_item)
+                    <span class="xe-badge btn-default">{{ $category_field_item }}</span>
+                @endforeach
+            @else
+                {{$category_field_items}}
+            @endif
         </div>
         @endif
     @endforeach
