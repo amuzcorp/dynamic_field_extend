d45fa1657992dc633a4ea4270071739824c1e019
zoozkwon <zooz@amuz.co.kr>
2022-02-16 19:46:36 +0900

    수정

--- src/DynamicFields/Location/Skins/DoroAPI/views/edit.blade.php
+++ src/DynamicFields/Location/Skins/DoroAPI/views/edit.blade.php
@@ -91,10 +91,9 @@
 
         htmlStr += `
         <tr>
-            <th>주소</th>
-            <th>동</th>
-            <th>영문주소</th>
             <th>우편번호</th>
+            <th>주소</th>
+            <th>지번</th>
             <th></th>
         </tr>
 
@@ -103,10 +102,9 @@
         // jquery를 이용한 JSON 결과 데이터 파싱
         $(jsonStr.results.juso).each(function(){
             htmlStr += "<tr>";
-            htmlStr += "<td>"+this.roadAddrPart1+"</td>";
-            htmlStr += "<td>"+this.roadAddrPart2+"</td>";
-            htmlStr += "<td>"+this.engAddr+"</td>";
             htmlStr += "<td>"+this.zipNo+"</td>";
+            htmlStr += "<td>"+this.roadAddrPart1+"</td>";
+            htmlStr += "<td>"+this.jibunAddr+"</td>";
             htmlStr += `<td>
                 <input type="button" onclick="addressing('${this.zipNo}', '${this.jibunAddr}','${this.roadAddrPart1}', '${configId}')" value="선택"></td>
             </tr>`;
@@ -196,65 +194,6 @@
             $("#pageApi").html(paggingStr);
         }
 
-    }
-
-    function execDaumPostcode_{{$config->get('id')}}() {
-        new daum.Postcode({
-            oncomplete: function(data) {
-                // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
-
-                // 도로명 주소의 노출 규칙에 따라 주소를 표시한다.
-                // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
-                var roadAddr = data.roadAddress; // 도로명 주소 변수
-                var extraRoadAddr = ''; // 참고 항목 변수
-
-                // 법정동명이 있을 경우 추가한다. (법정리는 제외)
-                // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
-                if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
-                    extraRoadAddr += data.bname;
-                }
-                // 건물명이 있고, 공동주택일 경우 추가한다.
-                if(data.buildingName !== '' && data.apartment === 'Y'){
-                    extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
-                }
-                // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
-                if(extraRoadAddr !== ''){
-                    extraRoadAddr = ' (' + extraRoadAddr + ')';
-                }
-
-                // 우편번호와 주소 정보를 해당 필드에 넣는다.
-                document.getElementById("postcode_{{$config->get('id')}}").value = data.zonecode;
-                document.getElementById("doro_{{$config->get('id')}}").value = roadAddr;
-                document.getElementById("jibun_{{$config->get('id')}}").value = data.jibunAddress;
-
-                // 참고항목 문자열이 있을 경우 해당 필드에 넣는다.
-                /*if(roadAddr !== ''){
-                    document.getElementById("extra_{{$config->get('id')}}").value = extraRoadAddr;
-                } else {
-                    document.getElementById("extra_{{$config->get('id')}}").value = '';
-                }*/
-
-                var guideTextBox = document.getElementById("guide_{{$config->get('id')}}");
-                // 사용자가 '선택 안함'을 클릭한 경우, 예상 주소라는 표시를 해준다.
-                if(data.autoRoadAddress) {
-                    var expRoadAddr = data.autoRoadAddress + extraRoadAddr;
-                    document.getElementById("doro_{{$config->get('id')}}").value = expRoadAddr;
-                    // guideTextBox.innerHTML = '(예상 도로명 주소 : ' + expRoadAddr + ')';
-                    // guideTextBox.style.display = 'block';
-
-                } else if(data.autoJibunAddress) {
-                    var expJibunAddr = data.autoJibunAddress;
-                    document.getElementById("jibun_{{$config->get('id')}}").value = expJibunAddr;
-                    // guideTextBox.innerHTML = '(예상 지번 주소 : ' + expJibunAddr + ')';
-                    // guideTextBox.style.display = 'block';
-                } else {
-                    guideTextBox.innerHTML = '';
-                    guideTextBox.style.display = 'none';
-                }
-
-                a2c_{{$config->get('id')}}(roadAddr);
-            }
-        }).open();
     }
 
     var mapContainer_{{ $config->get('id') }} = document.getElementById('map_{{ $config->get('id') }}'), // 지도를 표시할 div
