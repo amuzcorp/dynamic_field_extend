d5f00e640e5cd4ce745b59e2a5ab17341b5f4972
zoozkwon <zooz0806@gmail.com>
2022-07-25 12:33:57 +0900

    categoryInput ListItem 수정

--- src/DynamicFields/CategoryInput/views/list-item.blade.php
+++ src/DynamicFields/CategoryInput/views/list-item.blade.php
@@ -2,7 +2,11 @@
     @foreach($data as $key => $json_obj)
         @if($json_obj != null)
         <div class="{{ $key }}">
-            @php $category_field_items = json_dec($json_obj); @endphp
+            @if(!strpos($json_obj, '"'))
+                @php $category_field_items = $json_obj; @endphp
+            @else
+                @php $category_field_items = json_dec($json_obj) ?? $json_obj; @endphp
+            @endif
             @if(is_array($category_field_items) == true)
                 @foreach($category_field_items as $category_field_item)
                     <span class="xe-badge btn-default">{{ $category_field_item }}</span>
